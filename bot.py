50d760c02e203bac227081c2b9c9412362a0621c
Anon4You <alienkrishn@gmail.com>
2025-01-20 19:52:44 +0530

    telegram bot

--- bot.py
+++ bot.py
@@ -55,7 +55,7 @@ def send_ip_info(message):
                 f"🏢 *Organization:* {data['org']}\n"
                 f"🔗 *AS:* {data['as']}\n"
                 f"🌍 *Google Map:* https://maps.google.com/maps?q={data['lat']},{data['lon']}\n\n"
-                f"👤 *Created by:* [Alienkrishn](https://github.com/Alienkrishn)"
+                f"👤 *Created by:* [Alienkrishn](https://github.com/Anon4You)"
             )
         else:
             info = "Could not retrieve information for that IP address."
